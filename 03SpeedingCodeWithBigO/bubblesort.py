0724a0d3681c5d95fd3a59c07be64f317a4b7aea
Yuvraj-Singh-0505 <126414846+Yuvraj-Singh-0505@users.noreply.github.com>
2025-05-20 21:27:49 +0530

    task

---
+++ 03SpeedingCodeWithBigO/bubblesort.py
@@ -0,0 +1,27 @@
+arr = [4, 2, 7, 1, 3]
+n = len(arr)
+step = 1  
+
+print("Original array:", arr)
+
+for i in range(n):
+    swapped = False
+    print(f"\nPassthrough #{i+1}:")
+
+    for j in range(n - 1 - i):
+        print(f"Step #{step}: Compare {arr[j]} and {arr[j+1]}")
+        if arr[j] > arr[j+1]:
+            arr[j], arr[j+1] = arr[j+1], arr[j]
+            print(f"Step #{step+1}: Swap -> {arr}")
+            swapped = True
+            step += 1  
+        else:
+            print(f"Step #{step+1}: No swap -> {arr}")
+            step += 1
+        step += 1
+
+    if not swapped:
+        print("No swaps made in this pass — array is sorted.")
+        break
+
+print("\nSorted array:", arr)
